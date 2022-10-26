f000fc3b7037629377f865960b34dc450ac2f007
Fúlvio <fulviocanducci@hotmail.com>
2022-10-26 16:56:45 -0300

    first commit

---
+++ resources/js/Pages/Home/Index.jsx
@@ -0,0 +1,12 @@
+import { Head } from "@inertiajs/inertia-react";
+
+function Index() {
+    return (
+        <div>
+            <Head title="Home Page"></Head>
+            <div>aaaaaaa</div>
+        </div>
+    );
+}
+
+export default Index;
